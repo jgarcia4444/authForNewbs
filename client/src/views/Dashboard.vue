2d33caa913701d9a4cd12e5683461add7201a0d7
Jake Garcia <bballwiz4@gmail.com>
2019-01-17 23:12:46 -0800

    grab the notes through the backend that are associated with user._id that is logged in

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -33,6 +33,7 @@ export default {
             title: '',
             note: '',
         },
+        notes: [],
     }),
     mounted() {
         fetch(API_URL, {
@@ -58,7 +59,12 @@ export default {
                     authorization: `Bearer ${localStorage.token}`,
                 },
             }).then( res => res.json()).then( (note) => {
-                console.log(note);
+                this.notes.push(note);
+                this.newNote = {
+                    title: '',
+                    note: '',
+                };
+                this.showForm = false;
             });
         },
         logout() {
